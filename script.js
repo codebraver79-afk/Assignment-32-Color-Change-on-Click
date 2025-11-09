54e57dc9d4bbf1f884368ae281935f07dc56be3b
codebraver79-afk <codebraver79@codebrave.org>
2025-11-09 15:07:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('red').addEventListener("click",function(){
+       document.body.style.backgroundColor='red'
+     })
+document.getElementById('green').addEventListener("click",function(){
+       document.body.style.backgroundColor='green'
+     })
+document.getElementById('blue').addEventListener("click",function(){
+       document.body.style.backgroundColor='blue'
+     })
+document.getElementById('reset').addEventListener("click",function(){
+       document.body.style.backgroundColor='white'
+     })
